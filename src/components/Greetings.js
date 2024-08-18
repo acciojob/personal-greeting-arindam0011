8a95848bfeb8f9d5f0b4069b27b8522dd9360eec
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-08-18 16:41:20 +0530

    Update Greetings.js

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,18 +1,1 @@
-import React, { useState } from 'react'
 
-const Greetings = () => {
-    const[name, setName] = useState('')
-  return (
-    <div>
-      <label htmlFor='name'>Enter Your Name:</label><br/>
-      <input id='name' type="text" onChange={(e) => {
-        let greet= e.target.value ? "Hello " + e.target.value +"!" : "";
-        setName(greet)
-
-      }} />
-      <div>{name}</div>
-    </div>
-  )
-}
-
-export default Greetings
